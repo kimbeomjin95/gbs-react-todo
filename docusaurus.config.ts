a52bc89eb9ee60fcfa723bda7d615ea5e23a42c2
bjk <kimbeomjin95@gmail.com>
2025-10-07 21:25:11 -0400

    GitHub Pages 배포 설정 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://kimbeomjin95.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/gbs-react-todo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kimbeomjin95', // Usually your GitHub org/user name.
+  projectName: 'gbs-react-todo', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
